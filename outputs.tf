f32c05a48ebdae5ded4c73d7544233134f76086e
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:04 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "automation_job_schedules" {
-  description = "All automation_job_schedule resources"
-  value       = azurerm_automation_job_schedule.automation_job_schedules
-}
 output "automation_job_schedules_automation_account_name" {
-  description = "List of automation_account_name values across all automation_job_schedules"
-  value       = [for k, v in azurerm_automation_job_schedule.automation_job_schedules : v.automation_account_name]
+  description = "Map of automation_account_name values across all automation_job_schedules, keyed the same as var.automation_job_schedules"
+  value       = { for k, v in azurerm_automation_job_schedule.automation_job_schedules : k => v.automation_account_name }
 }
 output "automation_job_schedules_job_schedule_id" {
-  description = "List of job_schedule_id values across all automation_job_schedules"
-  value       = [for k, v in azurerm_automation_job_schedule.automation_job_schedules : v.job_schedule_id]
+  description = "Map of job_schedule_id values across all automation_job_schedules, keyed the same as var.automation_job_schedules"
+  value       = { for k, v in azurerm_automation_job_schedule.automation_job_schedules : k => v.job_schedule_id }
 }
 output "automation_job_schedules_parameters" {
-  description = "List of parameters values across all automation_job_schedules"
-  value       = [for k, v in azurerm_automation_job_schedule.automation_job_schedules : v.parameters]
+  description = "Map of parameters values across all automation_job_schedules, keyed the same as var.automation_job_schedules"
+  value       = { for k, v in azurerm_automation_job_schedule.automation_job_schedules : k => v.parameters }
 }
 output "automation_job_schedules_resource_group_name" {
-  description = "List of resource_group_name values across all automation_job_schedules"
-  value       = [for k, v in azurerm_automation_job_schedule.automation_job_schedules : v.resource_group_name]
+  description = "Map of resource_group_name values across all automation_job_schedules, keyed the same as var.automation_job_schedules"
+  value       = { for k, v in azurerm_automation_job_schedule.automation_job_schedules : k => v.resource_group_name }
 }
 output "automation_job_schedules_resource_manager_id" {
-  description = "List of resource_manager_id values across all automation_job_schedules"
-  value       = [for k, v in azurerm_automation_job_schedule.automation_job_schedules : v.resource_manager_id]
+  description = "Map of resource_manager_id values across all automation_job_schedules, keyed the same as var.automation_job_schedules"
+  value       = { for k, v in azurerm_automation_job_schedule.automation_job_schedules : k => v.resource_manager_id }
 }
 output "automation_job_schedules_run_on" {
-  description = "List of run_on values across all automation_job_schedules"
-  value       = [for k, v in azurerm_automation_job_schedule.automation_job_schedules : v.run_on]
+  description = "Map of run_on values across all automation_job_schedules, keyed the same as var.automation_job_schedules"
+  value       = { for k, v in azurerm_automation_job_schedule.automation_job_schedules : k => v.run_on }
 }
 output "automation_job_schedules_runbook_name" {
-  description = "List of runbook_name values across all automation_job_schedules"
-  value       = [for k, v in azurerm_automation_job_schedule.automation_job_schedules : v.runbook_name]
+  description = "Map of runbook_name values across all automation_job_schedules, keyed the same as var.automation_job_schedules"
+  value       = { for k, v in azurerm_automation_job_schedule.automation_job_schedules : k => v.runbook_name }
 }
 output "automation_job_schedules_schedule_name" {
-  description = "List of schedule_name values across all automation_job_schedules"
-  value       = [for k, v in azurerm_automation_job_schedule.automation_job_schedules : v.schedule_name]
+  description = "Map of schedule_name values across all automation_job_schedules, keyed the same as var.automation_job_schedules"
+  value       = { for k, v in azurerm_automation_job_schedule.automation_job_schedules : k => v.schedule_name }
 }
 
